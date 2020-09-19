671631d9caacc1151943d55960f064bfd8264108
bananasfoster623 <61858678+bananasfoster623@users.noreply.github.com>
2020-09-19 11:46:14 -0400

    Updated the PyPoll main.py to find unique candidate names.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,16 +6,24 @@ with open('.\\Resources\\election_data.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     data = list(reader)
 
-rows = len(data)
-cols = len(data[0])
+rows = len(data) ## This value -1 is our total number of votes cast
+cols = len(data[0]) ## This is our number of columns
 
-print(f'There are {rows} rows and {cols} columns in this dataset. ')
-print(f'The column names are: {data[0][0]} - {data[0][1]} - {data[0][2]}')
+candidateNames = []
+for i in range(1,len(data),1):
+    # if current (i,2) value is not in candidateNames list, append
+    if data[i][2] not in candidateNames:
+        candidateNames.append(data[i][2])
 
+voteCounts = []
+for i in range(len(candidateNames)):   
+    voteCounts.append(sum(x.count(candidateNames[i]) for x in data))
 
+votePercent = []
+for i in range(len(voteCounts)):
+    votePercent.append(voteCounts[i]/(rows-1))
 
-
-
+print(votePercent)
 
 # Prepare the terminal output
 print('\n\n') ### This is temporary until final formatting
