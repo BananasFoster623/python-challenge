3b229b84c0644c4146ea474280f251086dee8958
bananasfoster623 <61858678+bananasfoster623@users.noreply.github.com>
2020-09-19 11:18:50 -0400

    Added the terminal output formatting for PyPoll main.py. Also found the size of the dataset and column names.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,7 +6,27 @@ with open('.\\Resources\\election_data.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     data = list(reader)
 
-for i in range(len(data)):
-    for j in range(len(data[i])):
-        print(data[i][j])
-    print()
+rows = len(data)
+cols = len(data[0])
+
+print(f'There are {rows} rows and {cols} columns in this dataset. ')
+print(f'The column names are: {data[0][0]} - {data[0][1]} - {data[0][2]}')
+
+
+
+
+
+
+# Prepare the terminal output
+print('\n\n') ### This is temporary until final formatting
+print('Election Results')
+print('---------------------------------------------------------------')
+print(f'Total Votes: ')
+print('---------------------------------------------------------------')
+print(f'Khan:')
+print(f'Correy: ')
+print(f'Li: ')
+print(f'O\'Tooley: ')
+print('---------------------------------------------------------------')
+print(f'Winner: ')
+print('---------------------------------------------------------------')
